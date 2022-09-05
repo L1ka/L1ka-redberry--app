783cdafde24427c9fe0a84d811987d947204889f
L1ka <100157047+L1ka@users.noreply.github.com>
2022-09-05 22:10:42 +0400

    UserInfo.js - dropdowns
    renders separate component DropDown which on itself renders two dropdowns for team and position.
    
    has 2 state to save team and position data from api calls and passes it to DropDown  component. also passes onChange function to get changed values in UserInfo page. also passes errors state to be update in case of arrors

--- my-app/src/components/UserInfo.js
+++ my-app/src/components/UserInfo.js
@@ -9,6 +9,36 @@ export default function UserInfo() {
   const [formValues, setFormValues] = React.useState();
   const [formErrors, setFormErrors] = React.useState({});
 
+  const [team, setTeam] = React.useState([]);
+  const [position, setPosition] = React.useState([]);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("https://pcfy.redberryinternship.ge/api/teams");
+      const { data } = await res.json();
+
+      setTeam(data);
+    };
+    getData();
+  }, []);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const res = await fetch(
+        "https://pcfy.redberryinternship.ge/api/positions"
+      );
+      const { data } = await res.json();
+
+      setPosition(data);
+    };
+    getData();
+  }, []);
+
+  const onChange = function (prop) {
+    const { name, value } = prop.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   return (
     <div className="personal--info--container">
       <MainHeader id="user" handleSubmit={handleSubmit} />
@@ -45,6 +75,17 @@ export default function UserInfo() {
               ? formErrors.surname
               : "მინიმუმ 2 სიმბოლო, ქართული ასოები"}
           </h5>
+        </section>
+        <section className="testimonial">
+          <DropDown
+            name="team_id"           
+            onChange={onChange}
+            data={team}
+            data2={position}
+            title="თიმი"
+            formErrors={formErrors}
+            id="team_id"
+          />
         </section>
       </form>
     </div>
