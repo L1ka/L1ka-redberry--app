e30358ecedaf8f0c771684b976b5cd3b38e5bb86
L1ka <100157047+L1ka@users.noreply.github.com>
2022-09-05 22:56:23 +0400

    LaptopInfo.js - FormValue handling
    getFormValues function checks if in local storage is an object with values , if there is no data then in state default object will be empty object with input keys.
    
    if input changes value handleValueChange will be called to set State with new changes + old data.

--- my-app/src/components/LaptopInfo.js
+++ my-app/src/components/LaptopInfo.js
@@ -3,6 +3,41 @@ import { useNavigate } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import axios from "axios";
 export default function LaptopInfo(prop) {
+  const getFormValues = function () {
+    const storedValues = localStorage.getItem("save--onType--laptop");
+    if (!storedValues)
+      return {
+        laptop_name: "",
+        laptop_image: "",
+        laptop_brand_id: "",
+        laptop_cpu: "",
+        laptop_cpu_cores: "",
+        laptop_cpu_threads: "",
+        laptop_ram: "",
+        laptop_hard_drive_type: "",
+        laptop_state: "",
+        laptop_purchase_date: "",
+        laptop_price: "",
+      };
+    return JSON.parse(storedValues);
+  };
+  const [formValues, setFormValues] = React.useState(getFormValues);
+  const [formErrors, setFormErrors] = React.useState({});
+  const [isSubmit, setIsSubmit] = React.useState(false);
+
+  React.useEffect(() => {
+    localStorage.setItem("save--onType--laptop", JSON.stringify(formValues));
+  }, [formValues]);
+
+  const handleValueChange = function (e) {
+    const { name, value } = e.target;
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
   const [baseImage, setBaseImage] = React.useState({ selectedFile: null });
 
   const [image, setImage] = React.useState();
